d9e33a91e064d28c9e03dd57a3cade5888141636
AleshinNA <108576527+AleshinNA@users.noreply.github.com>
2022-09-07 23:18:14 +0500

    Add files via upload

---
+++ sq.sql.sql
@@ -0,0 +1,58 @@
+--------------------------------------------------------
+--  File created - среда-сент€бр€-07-2022   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence SQ_CATEGORIES
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "USER2022"."SQ_CATEGORIES"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 4 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SQ_CITY
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "USER2022"."SQ_CITY"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SQ_EDINICA_TOVARA
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "USER2022"."SQ_EDINICA_TOVARA"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 112 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SQ_EXEC_ORDERS
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "USER2022"."SQ_EXEC_ORDERS"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SQ_KOD_TOVARA
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "USER2022"."SQ_KOD_TOVARA"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 112 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SQ_ORDERS
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "USER2022"."SQ_ORDERS"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SQ_PRODUCT
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "USER2022"."SQ_PRODUCT"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 25 CACHE 2 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SQ_STATUS_ORDERS
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "USER2022"."SQ_STATUS_ORDERS"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 25 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SQ_STORES
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "USER2022"."SQ_STORES"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 29 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SQ_TYPE_RECEIPT_ORDER
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "USER2022"."SQ_TYPE_RECEIPT_ORDER"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence SQ_USERS
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "USER2022"."SQ_USERS"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 61 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
---
+++ tr.sql.sql
@@ -0,0 +1,146 @@
+--------------------------------------------------------
+--  File created - среда-сент€бр€-07-2022   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Trigger TR_SQ_CATEGORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE NONEDITIONABLE TRIGGER "USER2022"."TR_SQ_CATEGORIES" 
+  before INSERT or update ON categories FOR EACH ROW
+BEGIN
+if :new.id_categories is null then
+select sq_categories.nextval into :new.id_categories from dual;
+end if;
+end;
+/
+ALTER TRIGGER "USER2022"."TR_SQ_CATEGORIES" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger TR_SQ_CITY
+--------------------------------------------------------
+
+  CREATE OR REPLACE NONEDITIONABLE TRIGGER "USER2022"."TR_SQ_CITY" 
+  before INSERT or update ON city FOR EACH ROW
+BEGIN
+if :new.id_city is null then
+select sq_city.nextval into :new.id_city from dual;
+end if;
+end;
+/
+ALTER TRIGGER "USER2022"."TR_SQ_CITY" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger TR_SQ_EDINICA_TOVARA
+--------------------------------------------------------
+
+  CREATE OR REPLACE NONEDITIONABLE TRIGGER "USER2022"."TR_SQ_EDINICA_TOVARA" 
+  before INSERT or update ON edinica_tovara FOR EACH ROW
+BEGIN
+if :new.id_edinica_tovara is null then
+select sq_edinica_tovara.nextval into :new.id_edinica_tovara from dual;
+end if;
+end;
+/
+ALTER TRIGGER "USER2022"."TR_SQ_EDINICA_TOVARA" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger TR_SQ_EXEC_ORDERS
+--------------------------------------------------------
+
+  CREATE OR REPLACE NONEDITIONABLE TRIGGER "USER2022"."TR_SQ_EXEC_ORDERS" 
+  before INSERT or update ON exec_orders FOR EACH ROW
+BEGIN
+if :new.id_exec_orders is null then
+select sq_exec_orders.nextval into :new.id_exec_orders from dual;
+end if;
+end;
+/
+ALTER TRIGGER "USER2022"."TR_SQ_EXEC_ORDERS" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger TR_SQ_KOD_TOVARA
+--------------------------------------------------------
+
+  CREATE OR REPLACE NONEDITIONABLE TRIGGER "USER2022"."TR_SQ_KOD_TOVARA" 
+  before INSERT or update ON edinica_tovara FOR EACH ROW
+BEGIN
+if :new.kod_tovara is null then
+select sq_kod_tovara.nextval into :new.kod_tovara from dual;
+end if;
+end;
+/
+ALTER TRIGGER "USER2022"."TR_SQ_KOD_TOVARA" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger TR_SQ_ORDERS
+--------------------------------------------------------
+
+  CREATE OR REPLACE NONEDITIONABLE TRIGGER "USER2022"."TR_SQ_ORDERS" 
+  before INSERT or update ON orders FOR EACH ROW
+BEGIN
+if :new.id_order is null then
+select sq_orders.nextval into :new.id_order from dual;
+end if;
+end;
+/
+ALTER TRIGGER "USER2022"."TR_SQ_ORDERS" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger TR_SQ_PRODUCT
+--------------------------------------------------------
+
+  CREATE OR REPLACE NONEDITIONABLE TRIGGER "USER2022"."TR_SQ_PRODUCT" 
+  before INSERT or update ON product FOR EACH ROW
+BEGIN
+if :new.id_product is null then
+select sq_product.nextval into :new.id_product from dual;
+end if;
+end;
+/
+ALTER TRIGGER "USER2022"."TR_SQ_PRODUCT" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger TR_SQ_STATUS_ORDERS
+--------------------------------------------------------
+
+  CREATE OR REPLACE NONEDITIONABLE TRIGGER "USER2022"."TR_SQ_STATUS_ORDERS" 
+  before INSERT or update ON status_orders FOR EACH ROW
+BEGIN
+if :new.id_status_orders is null then
+select sq_status_orders.nextval into :new.id_status_orders from dual;
+end if;
+end;
+/
+ALTER TRIGGER "USER2022"."TR_SQ_STATUS_ORDERS" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger TR_SQ_STORES
+--------------------------------------------------------
+
+  CREATE OR REPLACE NONEDITIONABLE TRIGGER "USER2022"."TR_SQ_STORES" 
+  before INSERT or update ON stores FOR EACH ROW
+BEGIN
+if :new.id_stores is null then
+select sq_stores.nextval into :new.id_stores from dual;
+end if;
+end;
+/
+ALTER TRIGGER "USER2022"."TR_SQ_STORES" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger TR_SQ_TYPE_RECEIPT_ORDER
+--------------------------------------------------------
+
+  CREATE OR REPLACE NONEDITIONABLE TRIGGER "USER2022"."TR_SQ_TYPE_RECEIPT_ORDER" 
+  before INSERT or update ON type_receipt_order FOR EACH ROW
+BEGIN
+if :new.id_type_receipt_order is null then
+select sq_type_receipt_order.nextval into :new.id_type_receipt_order from dual;
+end if;
+end;
+/
+ALTER TRIGGER "USER2022"."TR_SQ_TYPE_RECEIPT_ORDER" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger TR_SQ_USERS
+--------------------------------------------------------
+
+  CREATE OR REPLACE NONEDITIONABLE TRIGGER "USER2022"."TR_SQ_USERS" 
+  before INSERT or update ON users FOR EACH ROW
+BEGIN
+if :new.id_user is null then
+select sq_users.nextval into :new.id_user from dual;
+end if;
+end;
+/
+ALTER TRIGGER "USER2022"."TR_SQ_USERS" ENABLE;
